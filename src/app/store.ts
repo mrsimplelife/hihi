f08267cc04d0d6cd7810b3f01567ba7adb60ab92
mrsimplelife <mrsimplelife@naver.com>
2022-05-22 23:15:39 +0900

    4 files modified

--- src/app/store.ts
+++ src/app/store.ts
@@ -12,7 +12,29 @@ import {
 } from 'redux-persist';
 import { useMemo } from 'react';
 
-import storage from 'redux-persist/lib/storage';
+// import storage from 'redux-persist/lib/storage';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key: string) {
+      return Promise.resolve(null);
+    },
+    setItem(_key: string, value: string) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key: string) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('local')
+    : createNoopStorage();
+
+export default storage;
 
 const persistConfig = {
   key: 'primary',
